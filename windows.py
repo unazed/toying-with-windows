0e76fcb66c6a1171d52eb04c45d50afc9306d7aa
Unazed Spectaculum <leethax0r.coder@gmail.com>
2018-09-02 15:38:38 +0100

    get_computer_name[_ex] functions
    simplified length pointers with the `len_pointer` function

--- windows.py
+++ windows.py
@@ -50,10 +50,119 @@ STD_CLIPBOARD_FORMATS = {
     "CF_UNICODETEXT":     13,
     "CF_WAVE":            12
     }
+SYS_METRICS_INDICES = {
+    "SM_ARRANGE":                     56,
+    "SM_CLEANBOOT":                   67,
+    "SM_CMONITORS":                   80,
+    "SM_CMOUSEBUTTONS":               43,
+    "SM_CONVERTIBLESLATEMODE":        0x2003,
+    "SM_CXBORDER":                    5,
+    "SM_CXCURSOR":                    13,
+    "SM_CXDLGFRAME":                  7,
+    "SM_CXDOUBLECLK":                 36,
+    "SM_CXDRAG":                      68,
+    "SM_CXEDGE":                      45,
+    "SM_CXFIXEDFRAME":                7,
+    "SM_CXFOCUSBORDER":               83,
+    "SM_CXFRAME":                     32,
+    "SM_CXFULLSCREEN":                16,
+    "SM_CXHSCROLL":                   21,
+    "SM_CXHTHUMB":                    10,
+    "SM_CXICON":                      11,
+    "SM_CXICONSPACING":               38,
+    "SM_CXMAXIMIZED":                 61,
+    "SM_CXMAXTRACK":                  59,
+    "SM_CXMENUCHECK":                 71,
+    "SM_CXMENUSIZE":                  54,
+    "SM_CXMIN":                       28,
+    "SM_CXMINIMIZED":                 57,
+    "SM_CXMINSPACING":                47,
+    "SM_CXMINTRACK":                  34,
+    "SM_CXPADDEDBORDER":              92,
+    "SM_CXSCREEN":                    0,
+    "SM_CXSIZE":                      30,
+    "SM_CXSIZEFRAME":                 32,
+    "SM_CXSMICON":                    49,
+    "SM_CXSMSIZE":                    52,
+    "SM_CXVIRTUALSCREEN":             78,
+    "SM_CXVSCROLL":                   2,
+    "SM_CYBORDER":                    6,
+    "SM_CYCAPTION":                   4,
+    "SM_CYCURSOR":                    14,
+    "SM_CYDLGFRAME":                  8,
+    "SM_CYDOUBLECLK":                 37,
+    "SM_CYDRAG":                      69,
+    "SM_CYEDGE":                      46,
+    "SM_CYFIXEDFRAME":                8,
+    "SM_CYFOCUSBORDER":               84,
+    "SM_CYFRAME":                     33,
+    "SM_CYFULLSCREEN":                17,
+    "SM_CYHSCROLL":                   3,
+    "SM_CYICON":                      12,
+    "SM_CYICONSPACING":               39,
+    "SM_CYKANJIWINDOW":               18,
+    "SM_CYMAXIMIZED":                 62,
+    "SM_CYMAXTRACK":                  60,
+    "SM_CYMENU":                      16,
+    "SM_CYMENUCHECK":                 72,
+    "SM_CYMENUSIZE":                  55,
+    "SM_CYMIN":                       29,
+    "SM_CYMINIMIZED":                 58,
+    "SM_CYMINSPACING":                48,
+    "SM_CYMINTRACK":                  35,
+    "SM_CYSCREEN":                    1,
+    "SM_CYSIZE":                      31,
+    "SM_CYSIZEFRAME":                 33,
+    "SM_CYSMCAPTION":                 51,
+    "SM_CYSMICON":                    50,
+    "SM_CYSMSIZE":                    53,
+    "SM_CYVIRTUALSCREEN":             79,
+    "SM_CYVSCROLL":                   20,
+    "SM_CYVTHUMB":                    9,
+    "SM_DBCSENABLED":                 42,
+    "SM_DEBUG":                       22,
+    "SM_DIGITIZER":                   94,
+    "SM_IMMENALED":                   82,
+    "SM_MAXIMUMTOUCHES":              95,
+    "SM_MEDIACENTER":                 87,
+    "SM_MENUDROPALIGNMENT":           40,
+    "SM_MIDEASTENABLED":              74,
+    "SM_MOUSEPRESENT":                19,
+    "SM_MOUSEHORIZONTALWHEELPRESENT": 91,
+    "SM_MOUSEWHEELPRESENT":           75,
+    "SM_NETWORK":                     63,
+    "SM_PENWINDOWS":                  41,
+    "SM_REMOTECONTROL":               0x2001,
+    "SM_REMOTESESSION":               0x1000,
+    "SM_SAMEDISPLAYFORMAT":           81,
+    "SM_SECURE":                      44,
+    "SM_SERVERR2":                    89,
+    "SM_SHOWSOUNDS":                  70,
+    "SM_SHUTTINGDOWN":                0x2000,
+    "SM_SLOWMACHINE":                 73,
+    "SM_STARTER":                     88,
+    "SM_SWAPBUTTON":                  23,
+    "SM_SYSTEMDOCKED":                0x2004,
+    "SM_TABLETPC":                    86,
+    "SM_XVIRTUALSCREEN":              76,
+    "SM_YVIRTUALSCREEN":              77
+    }
+COMPUTER_NAME_FORMAT = {v: k for k, v in enumerate([
+    "ComputerNameNetBIOS",
+    "ComputerNameDnsHostname",
+    "ComputerNameDnsDomain",
+    "ComputerNameDnsFullyQualified",
+    "ComputerNamePhysicalNetBIOS",
+    "ComputerNamePhysicalDnsHostname",
+    "ComputerNamePhysicalDnsDomain",
+    "ComputerNamePhysicalDnsFullyQualified",
+    "ComputerNameMax"  # unused
+    ])}
 
 UNICODE = True
-
+TCHAR = ctypes.c_wchar if UNICODE else ctypes.c_char
 function_cache = {}
+len_pointer = lambda data, ctype: ctypes.pointer(ctype(len(data)))
 
 class POINT(ctypes.Structure):
     _fields_ = [
@@ -223,6 +332,7 @@ def get_std_handle(std_handle, _ctypes_configuration=(
     return get_std_handle(
         ctypes_configuration_param_select(_ctypes_configuration, 0) or std_handle
     )
+
 def write_console(handle, data, _ctypes_configuration=(
                   ("hConsoleOutput", (ctypes.c_voidp, True)),
                   ("lpBuffer", (ctypes.c_voidp, True)),
@@ -243,6 +353,7 @@ def write_console(handle, data, _ctypes_configuration=(
         _ctypes_configuration[3][1][1](),
         _ctypes_configuration[4][1][1]
     )
+
 def set_console_title(name, _ctypes_configuration=(
                       ("lpConsoleTitle", (ctypes.POINTER(ctypes.c_wchar), True)),
                      )):
@@ -256,7 +367,74 @@ def set_console_title(name, _ctypes_configuration=(
         ctypes_configuration_param_select(_ctypes_configuration, 0) or name
     )
 
+def read_console(handle, data_pointer, read_count, _ctypes_configuration=(
+                 ("hConsoleInput", (ctypes.c_voidp, True)),
+                 ("lpBuffer", (ctypes.c_voidp, True)),
+                 ("nNumberOfCharsToRead", (ctypes.c_ulong, True)),
+                 ("lpNumberOfCharsRead", (ctypes.c_voidp, lambda: ctypes.pointer(ctypes.c_ulong()))),
+                 ("pInputControl", (ctypes.c_voidp, None))
+                )):
+    read_console = import_winapi_function(
+        ctypes.windll.kernel32,
+        "ReadConsole",
+        argtypes_from_ctypes_configuration(_ctypes_configuration),
+        ctypes.c_ushort
+    )
+    return read_console(
+        ctypes_configuration_param_select(_ctypes_configuration, 0) or handle,
+        ctypes_configuration_param_select(_ctypes_configuration, 1) or data_pointer,
+        ctypes_configuration_param_select(_ctypes_configuration, 2) or read_count,
+        _ctypes_configuration[3][1][1](),
+        _ctypes_configuration[4][1][1]
+    )
+
+def get_system_metrics(idx, _ctypes_configuration=(
+                       ("nIndex", (ctypes.c_int, True)),
+                      )):
+    get_system_metrics = import_winapi_function(
+        ctypes.windll.user32,
+        "GetSystemMetrics",
+        argtypes_from_ctypes_configuration(_ctypes_configuration),
+        ctypes.c_int
+    )
+    return get_system_metrics(
+        ctypes_configuration_param_select(_ctypes_configuration, 0) or idx
+    )
+
+def get_computer_name(data, _ctypes_configuration=(
+                      ("lpBuffer", (ctypes.POINTER(TCHAR), True)),
+                      ("nSize", (ctypes.POINTER(ctypes.c_ulong), lambda d: len_pointer(d, ctypes.c_ulong)))
+                     )):
+    get_computer_name = import_winapi_function(
+        ctypes.windll.kernel32,
+        "GetComputerName",
+        argtypes_from_ctypes_configuration(_ctypes_configuration),
+        ctypes.c_ushort
+    )
+    d = ctypes_configuration_param_select(_ctypes_configuration, 0) or data
+    # prevent repetition
+    return get_computer_name(
+        d,
+        _ctypes_configuration[1][1][1](d)
+    )
+
+def get_computer_name_ex(name_type, data_pointer, _ctypes_configuration=(
+                         ("NameType", (ctypes.c_uint, True)),
+                         ("lpBuffer", (ctypes.POINTER(TCHAR), True)),
+                         ("lpnSize", (ctypes.POINTER(ctypes.c_ushort), lambda d: len_pointer(d, ctypes.c_ushort)))
+                        )):
+    get_computer_name_ex = import_winapi_function(
+        ctypes.windll.kernel32,
+        "GetComputerNameEx",
+        argtypes_from_ctypes_configuration(_ctypes_configuration),
+        ctypes.c_int
+    )
+    return get_computer_name_ex(
+        ctypes_configuration_param_select(_ctypes_configuration, 0) or name_type,
+        ctypes_configuration_param_select(_ctypes_configuration, 1) or data_pointer,
+        _ctypes_configuration[2][1][1](data_pointer)
+    )
+
+
 if __name__ == "__main__":
-    stdout = debug_fn(get_std_handle, 2**32 - 11)
-    debug_fn(set_console_title, "skid_program_v2")
-    debug_fn(write_console, stdout, create_string("hello skid !"))
+    pass  # all functions work!
